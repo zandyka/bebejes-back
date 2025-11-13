f1ec62a42bbd70b0b84b0b73fc2a565b344df0e8
zandyka <zackyandyka1@gmail.com>
2025-11-13 16:27:00 +0700

    Fix: Renamed problematic migration to bypass cache

--- database/migrations/2024_01_01_fix_add_coordinator_to_users_table.php
+++ database/migrations/2024_01_01_fix_add_coordinator_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class FixAddCoordinatorToUsersTable extends Migration
+class FixsAddCoordinatorToUsersTable extends Migration
 {
     public function up()
     {
